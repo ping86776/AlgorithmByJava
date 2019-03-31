d69c79efc32a34ca67f5c412acf13778eaa30c1a
ping86776 <ping86776@163.com>
2019-03-31 21:49:53 +0800

    找出数组重复最多的数

--- src/Others/NRepeated.java
+++ src/Others/NRepeated.java
@@ -11,20 +11,14 @@ public class NRepeated {
         int count  = A.length/2;
         int sum;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < A.length - 1; i++) {
-            if (map.containsKey(A[i]))  continue;
-            for (int j = i + 1; j < A.length; j++) {
-                if (A[i]==A[j]){
-                    if (!map.containsKey(A[i])){
-                        map.put(A[i],2);
-                    }else {
-                        sum = map.get(A[i]) + 1;
-                        map.put(A[i], sum);
-                    }
-                }
+        for (int i = 0; i < A.length; i++) {
+            if (!map.containsKey(A[i])){
+                map.put(A[i],1);
+            }else{
+                sum = map.get(A[i]) + 1;
+                map.put(A[i], sum);
             }
         }
-        System.out.println(map.entrySet());
         for (int i : map.keySet()){
             if (map.get(i)==count) return i;
         }
@@ -34,7 +28,6 @@ public class NRepeated {
     public static void main(String[] args) {
         NRepeated nRepeated = new NRepeated();
         int[] arr = {5,2,5,2,5,3,5,3};
-//        int[] arr = {1,2,3,3};
         System.out.println(nRepeated.repeatedNTimes(arr));
     }
 }
