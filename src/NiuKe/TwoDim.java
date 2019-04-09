243c4d7c8e9323c1359760dbfbb9c53d996faf47
ping86776 <ping86776@163.com>
2019-04-09 15:43:43 +0800

    二维数组的查找(网易笔试)

--- src/NiuKe/TwoDim.java
+++ src/NiuKe/TwoDim.java
@@ -5,20 +5,20 @@ package NiuKe;
  */
 
 /**
- * ��ά����Ĳ��ң���һ����ά�����У�ÿ��һά����ĳ�����ͬ����ÿһ�ж����մ����ҵ�����˳������
- * ÿһ�ж����մ��ϵ��µ�����˳�����������һ������������������һ����ά�����һ��������
- * �ж��������Ƿ��и�������
+ * 二维数组的查找：在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 判断数组中是否含有该整数。
  */
 public class TwoDim {
     public static boolean Find (int[][] arr, int n){
-        int row = 0;    //������
-        int col = arr[0].length-1;    //������
+        int row = 0;    //行索引
+        int col = arr[0].length-1;    //列索引
         while (row<=arr.length-1&&col>=0){
             if (n==arr[row][col]){
                 return true;
-            }else if (n<arr[row][col]){     //n�ȵ�ǰλ��С����һ���������
+            }else if (n<arr[row][col]){     //n比当前位置小，则一定在其左边
                 col--;
-            }else if (n>arr[row][col]){     //n�ȵ�ǰλ�ô���һ�������±�
+            }else if (n>arr[row][col]){     //n比当前位置大，则一定在其下边
                 row++;
             }
         }
