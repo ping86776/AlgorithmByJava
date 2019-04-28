25855f3566d80a4e99956987782049e456db8094
ping86776 <ping86776@163.com>
2019-04-28 19:04:28 +0800

    反转链表

--- src/Leetcode/LinkedList/ReverseLinkedList.java
+++ src/Leetcode/LinkedList/ReverseLinkedList.java
@@ -2,9 +2,12 @@ package Leetcode.LinkedList;
 
 /**
  * 206. Reverse Linked List Created by Ping on 2019/4/28.
- * 主要思想是将链表前指针和后指针分别存临时变量，当前指针的next赋值为前指针，前指针赋值为当前指针，当前指针赋值为next指针
+ *
  */
 public class ReverseLinkedList {
+    //遍历实现
+    //主要思想是将链表前指针和后指针分别存临时变量，
+    //当前指针的next赋值为前指针，前指针赋值为当前指针，当前指针赋值为next指针
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;
@@ -15,5 +18,19 @@ public class ReverseLinkedList {
             cur = NextTemp;
         }
         return pre;
+    }
+
+    //递归实现
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = reverseList2(head.next);
+
+        //这里只需要考虑要交换的两个数，递归只是重复一个函数
+        head.next.next = head;
+        head.next = null;
+
+        return cur;
+
+
     }
 }
