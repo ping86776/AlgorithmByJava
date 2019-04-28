bfe2a8451a847204017d7031854b332831686808
ping86776 <ping86776@163.com>
2019-04-28 14:53:41 +0800

    二叉树DFS栈实现

---
+++ src/Leetcode/BinaryTrees/BFS.java
@@ -0,0 +1,23 @@
+package Leetcode.BinaryTrees;
+
+import java.util.LinkedList;
+
+/**
+ * 广度优先搜索(层次遍历) Created by Ping on 2019/4/28.
+ */
+public class BFS {
+    public void Layertraversal(TreeNode root){
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.removeFirst();
+            System.out.println(node.val);
+            if (node.left!=null){
+                queue.addLast(node.left);
+            }
+            if (node.right!=null){
+                queue.addLast(node.right);
+            }
+        }
+    }
+}
