96c1a3823356c89038f2b83f8a156222c756be85
ping86776 <ping86776@163.com>
2019-04-30 19:33:06 +0800

    反转链表

--- src/Leetcode/LinkedList/ReverseLinkedList.java
+++ src/Leetcode/LinkedList/ReverseLinkedList.java
@@ -33,4 +33,15 @@ public class ReverseLinkedList {
 
 
     }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l1;
+        if(l2==null) return l2;
+        ListNode temp = l1.next;
+        l1.next = l2.next;
+
+        mergeTwoLists(l1.next,l2.next);
+
+        return l1;
+    }
 }
