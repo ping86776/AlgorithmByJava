e0024a6aa891c8036afe72976170dfa17356fc29
ping86776 <ping86776@163.com>
2019-07-20 18:20:29 +0800

    合并有序链表的递归非递归解法

--- src/Leetcode/LinkedList/MergeTwoLists.java
+++ src/Leetcode/LinkedList/MergeTwoLists.java
@@ -4,6 +4,7 @@ package Leetcode.LinkedList;
  * 21. 合并两个有序链表
  */
 public class MergeTwoLists {
+    //递归
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
@@ -18,21 +19,40 @@ public class MergeTwoLists {
         }
 
     }
+    //迭代
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        //前置节点
+        ListNode pre = new ListNode(0);
+        //复制pre指针，操作由cur指针完成，所有操作都会赋值到pre指针保存以往的状态
+        ListNode cur = pre;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+
+            cur = cur.next;
+
+        }
+        cur.next = l1 == null ? l2 : l1;
+
+        return pre.next;
+    }
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
         l1.next.next = new ListNode(3);
-        l1.next.next.next = new ListNode(4);
-        l1.next.next.next.next = new ListNode(5);
 
-        ListNode l2 = new ListNode(6);
-        l2.next = new ListNode(7);
-        l2.next.next = new ListNode(8);
-        l2.next.next.next = new ListNode(9);
-        l2.next.next.next.next = new ListNode(10);
+        ListNode l2 = new ListNode(4);
+        l2.next = new ListNode(5);
+        l2.next.next = new ListNode(6);
 
-        ListNode cur = MergeTwoLists.mergeTwoLists(l1,l2);
+        ListNode cur = MergeTwoLists.mergeTwoLists2(l1,l2);
         System.out.println(cur);
     }
 }
