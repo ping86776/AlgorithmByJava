6ea05e86c28de793ed798b7e310739e9b1288ffa
ping86776 <ping86776@163.com>
2019-07-25 11:24:36 +0800

    226. Invert Binary Tree 反转二叉树

--- src/Leetcode/BinaryTrees/InvertBinaryTree.java
+++ src/Leetcode/BinaryTrees/InvertBinaryTree.java
@@ -5,17 +5,35 @@ package Leetcode.BinaryTrees;
  * 226. Invert Binary Tree 反转二叉树
  */
 public class InvertBinaryTree {
-    public TreeNode invertTree(TreeNode root) {
+    public static TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
+        if (root.left == null && root.right == null) return null;
         //前序
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-
-        invertTree(root.left);
-        invertTree(root.right);
+        if (root.left != null){
+            invertTree(root.left);
+        }
+        if (root.right != null){
+            invertTree(root.right);
+        }
 
         return root;
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(0);
+        node.left = new TreeNode(1);
+        node.right = new TreeNode(2);
+        node.left.left = new TreeNode(3);
+        node.left.right = new TreeNode(4);
+        node.right.left = new TreeNode(5);
+        node.right.right = new TreeNode(6);
+
+        TreeNode p = InvertBinaryTree.invertTree(node);
+        System.out.println(p);
 
     }
 }
