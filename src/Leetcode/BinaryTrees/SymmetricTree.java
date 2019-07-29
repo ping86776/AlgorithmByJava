7b020b8d352d21231c5df9b968fb45d173738cf9
ping86776 <ping86776@163.com>
2019-07-29 14:32:37 +0800

    101. 对称二叉树

--- src/Leetcode/BinaryTrees/SymmetricTree.java
+++ src/Leetcode/BinaryTrees/SymmetricTree.java
@@ -13,7 +13,7 @@ public class SymmetricTree {
     private boolean isMirror(TreeNode left, TreeNode right){
         if (left == null && right == null) return true;
         if (left == null || right == null) return false;
-
+        //左子树的左子树和右子树的右子树相等且左子树的右子树和右子树的左子树相等
         return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
