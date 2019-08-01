cb5cfb7d498aff73359b67a4f2ffe713c5073360
ping86776 <ping86776@163.com>
2019-08-01 14:36:37 +0800

    调整数组顺序使奇数位于偶数前面

--- src/Leetcode/SortArrayByParity.java
+++ src/Leetcode/SortArrayByParity.java
@@ -8,25 +8,44 @@ import java.util.List;
  * 905. Sort Array By Parity 按奇偶排序数组 Created by Ping on 2019/3/25.
  */
 public class SortArrayByParity {
+    //思路一: 遍历A，将奇偶数分别取出来，最后将奇数放在偶数后
     public int[] sortArrayByParity(int[] A) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        List<Integer> oddnumber = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
         for (int i = 0; i < A.length; i++) {
-            if (A[i]%2==0){
-                list.add(A[i]);
-            }else list2.add(A[i]);
+            if ((A[i] & 1) == 0){
+                even.add(A[i]);
+            }else oddnumber.add(A[i]);
         }
-        list.addAll(list2);
-        int[] res = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            res[i]=list.get(i);
+        even.addAll(oddnumber);
+        int[] res = new int[even.size()];
+        for (int i = 0; i < even.size(); i++) {
+            res[i] = even.get(i);
         }
         return res;
     }
 
+    //思路二: 双指针法，左指针如果为奇数，右指针如果为偶数，则互换
+    public int[] sortArrayByParity2(int[] A){
+        if(A == null || A.length == 1) return A;
+        int left = 0, right = A.length-1;
+        while (left < right) {
+            if ((A[left] & 1) == 1 && (A[right] & 1) == 0) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+            }else if ((A[left] & 1) == 0) {
+                left++;
+            }else if ((A[right] & 1) == 1) {
+                right--;
+            }
+        }
+
+        return A;
+    }
     public static void main(String[] args) {
         int[] A = {3,1,2,4};
         SortArrayByParity sortArrayByParity = new SortArrayByParity();
-        System.out.println(Arrays.toString(sortArrayByParity.sortArrayByParity(A)));
+        System.out.println(Arrays.toString(sortArrayByParity.sortArrayByParity2(A)));
     }
 }
